2cead2e04f910e3cbff06f9fc3698b903c52400a
bsaptarshi <bsaptarshi52@LAPTOP-M2NUETBS.localdomain>
2024-01-14 19:50:05 -0800

    Workspace setup; MongoDB Config

---
+++ src/main/java/io/quantumdrive/dataleap/DataleapApplication.java
@@ -0,0 +1,13 @@
+package io.quantumdrive.dataleap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataleapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataleapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/quantumdrive/dataleap/DataleapApplicationTests.java
@@ -0,0 +1,13 @@
+package io.quantumdrive.dataleap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataleapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
